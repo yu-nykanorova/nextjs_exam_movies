79c4070a6228931bc43743bf4931391e1af06988
yu-nykanorova <juliyasos@ukr.net>
2026-04-24 18:51:27 +0300

    fixed pagination

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -11,7 +11,7 @@ export const Pagination = ({totalPages}: {totalPages: number}) => {
     const currentPage = Number(searchParams.get("page")) || 1;
 
     const handleClickPrev = () => {
-        if (currentPage > 1) {
+        if (currentPage > 1 && currentPage <= totalPages) {
             const newParams = new URLSearchParams(String(searchParams));
             newParams.set("page", String(currentPage - 1));
             router.push(`/?${newParams.toString()}`);
@@ -27,22 +27,25 @@ export const Pagination = ({totalPages}: {totalPages: number}) => {
     }
 
     return (
-        <div className="max-w-100 mx-auto my-8 p-4 flex items-center justify-center gap-6">
-            <button
-                className="px-3 py-1 flex items-center justify-center text-brand-black text-md bg-brand-light-blue rounded-md shadow-md enabled:hover:bg-brand-white transition disabled:opacity-40 disabled:cursor-not-allowed disabled:shadow-none"
-                onClick={handleClickPrev}
-                disabled={currentPage === 1 || currentPage > MAX_PAGES}
-            >
-                Prev
-            </button>
-            <div>{currentPage <= MAX_PAGES ? currentPage : 0} / {totalPages <= MAX_PAGES ? totalPages : MAX_PAGES}</div>
-            <button
-                className="px-3 py-1 flex items-center justify-center text-brand-black text-md bg-brand-light-blue rounded-md shadow-md enabled:hover:bg-brand-white transition disabled:opacity-40 disabled:cursor-not-allowed disabled:shadow-none"
-                onClick={handleClickNext}
-                disabled={currentPage >= totalPages || currentPage === MAX_PAGES}
-            >
-                Next
-            </button>
-        </div>
+        <>
+            {currentPage > totalPages && (<div className="text-red-400 text-center">No more pages in this category</div>)}
+            <div className="max-w-100 mx-auto my-8 p-4 flex items-center justify-center gap-6">
+                <button
+                    className="px-3 py-1 flex items-center justify-center text-brand-black text-md bg-brand-light-blue rounded-md shadow-md enabled:hover:bg-brand-white transition disabled:opacity-40 disabled:cursor-not-allowed disabled:shadow-none"
+                    onClick={handleClickPrev}
+                    disabled={currentPage === 1 || currentPage > totalPages}
+                >
+                    Prev
+                </button>
+                <div>{currentPage <= totalPages ? currentPage : 0} / {totalPages <= MAX_PAGES ? totalPages : MAX_PAGES}</div>
+                <button
+                    className="px-3 py-1 flex items-center justify-center text-brand-black text-md bg-brand-light-blue rounded-md shadow-md enabled:hover:bg-brand-white transition disabled:opacity-40 disabled:cursor-not-allowed disabled:shadow-none"
+                    onClick={handleClickNext}
+                    disabled={currentPage >= totalPages || currentPage === MAX_PAGES}
+                >
+                    Next
+                </button>
+            </div>
+        </>
     );
 };
